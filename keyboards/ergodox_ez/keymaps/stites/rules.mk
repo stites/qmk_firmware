2495b659ddee24e6f5a99b2dbddae35939ae6d74
Sam Stites <stites@users.noreply.github.com>
2019-10-24 21:48:04 -0400

    get plover keymap working!

--- keyboards/ergodox_ez/keymaps/stites/rules.mk
+++ keyboards/ergodox_ez/keymaps/stites/rules.mk
@@ -1,3 +1,7 @@
 STENO_ENABLE = yes          # Additional protocols for Stenography(+1700), requires VIRTSER
 # Not enough interupts, so something has to go
 MOUSEKEY_ENABLE = no
+
+# additional from https://paulfioravanti.com/blog/2018/10/18/starting-stenography-with-an-ergodox/
+VIRTSER_ENABLE = yes
+FORCE_NKRO = yes
